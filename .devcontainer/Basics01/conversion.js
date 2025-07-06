88121d3ac3c9d12be31976ffe6105b51656c865c
Aarjit95 <86615147+Aarjit95@users.noreply.github.com>
2025-07-06 12:43:44 +0000

    conversion basic complete

---
+++ .devcontainer/Basics01/conversion.js
@@ -0,0 +1,22 @@
+let score = true
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+let isLoggedin = 1
+console.log(typeof isLoggedin);
+let booleanIsLoggedIn = Boolean (isLoggedin);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn)
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
+
+// "33" => 3
+// "33abc" => NaN
