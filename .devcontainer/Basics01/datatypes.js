c0d5a144f314912df0860b3173b5584c8fa7042c
Aarjit95 <86615147+Aarjit95@users.noreply.github.com>
2025-07-06 12:26:07 +0000

    datype committed

---
+++ .devcontainer/Basics01/datatypes.js
@@ -0,0 +1,19 @@
+let name1 = "Aarjit"
+let age = 20
+let isLoggedin = true
+
+console.table([name1, age,isLoggedin]);
+// creating different datatypes
+
+//  number
+// string
+// boolean
+//bigint
+// null
+// undefined
+//symbol => unique
+
+// object 
+
+console.log(typeof undefined);
+
