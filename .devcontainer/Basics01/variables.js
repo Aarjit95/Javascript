bd8b0aa4133fcdbd345df680d14e823cf7fec414
Aarjit95 <86615147+Aarjit95@users.noreply.github.com>
2025-07-06 12:16:50 +0000

    Test and variables created

---
+++ .devcontainer/Basics01/variables.js
@@ -0,0 +1,10 @@
+const accountId = 1234
+let accountEmail = "aarjit.karun@gmail.com"
+var accountPassword = "12321"
+accountCity = "Jaipur"
+let accountState;
+
+
+console.log(accountId);
+console.log(accountEmail);
+console.table ([accountId,accountEmail,accountPassword, accountCity,accountState]);
